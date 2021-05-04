ef949e37feb34eed8d38ad7735354ca050c11a1e
John Hoffer <jhoffer@purestorage.com>
2021-05-04 16:52:33 -0500

    adding sequencer simulator

---
+++ roles/sim_sequencer/files/create_loop.ps1
@@ -0,0 +1,21 @@
+﻿# create loop
+Param (
+  [int]$count = 1000,
+  [string][Parameter(Mandatory = $true )]$targetdirectory
+  )
+
+Start-Transcript -path output.txt -append
+
+for ( $thiscount = 0; $thiscount -lt $count; $thiscount++ ) {
+    $filecount = '{0:d6}' -f [int]$thiscount
+    Write-Host Write: $thiscount
+    Set-Content -Path $targetdirectory\FILE$filecount.OUT -Value ( acgt )
+}
+
+for ( $thiscount = 0; $thiscount -lt $count; $thiscount++ ) {
+    $filecount = '{0:d6}' -f [int]$thiscount
+    Write-Host Read: $thiscount
+    Get-FileHash -Algorithm MD5 $targetdirectory\FILE$filecount.OUT
+}
+
+Stop-Transcript
---
+++ roles/sim_sequencer/files/multithread_create.ps1
@@ -0,0 +1,19 @@
+﻿#  .\multithread_create.ps1 -count 100 -basepath "Y:\$(Get-Date -Format FileDateTime)\"
+
+Param (
+  [int]$count = 1000,
+  [string][Parameter(Mandatory = $true )]$basepath
+  )
+
+$threads = (Get-WmiObject Win32_ComputerSystem).NumberofLogicalProcessors
+
+Write-Host "Starting $threads parallels jobs at $(Get-Date)."
+
+for ( $i = 0; $i -lt $threads; $i++ ) {
+    $thisdir = "RUN$i"
+    $thispath = $basepath + $thisdir
+    if ( -not ( Test-Path -Path $thispath ) ) {
+        New-Item -Path $basepath -Name $thisdir -ItemType "directory"
+    }
+            Start-Process -FilePath "powershell" -RedirectStandardOutput stdout.txt -RedirectStandardError stderr.txt -ArgumentList "-file c:\scripts\create_loop.ps1 -targetdirectory $thispath -count $count"
+}
